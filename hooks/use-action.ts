36e281aaf552e17411cae1f79a2d3aea1675d2d0
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2024-05-12 12:15:25 +0300

    Add board popover form

--- hooks/use-action.ts
+++ hooks/use-action.ts
@@ -1,9 +1,9 @@
-
 import { useState, useCallback } from "react";
+
 import { ActionState, FieldErrors } from "@/lib/create-safe-action";
 
-type Action<TInput, TOutput> = (data:TInput) => Promise<ActionState<TInput, TOutput>>
 
+type Action<TInput, TOutput> = (data: TInput) => Promise<ActionState<TInput, TOutput>>;
 
 interface UseActionOptions<TOutput> {
     onSuccess?: (data: TOutput) => void;
@@ -11,46 +11,52 @@ interface UseActionOptions<TOutput> {
     onComplete?: () => void;
 };
 
-export const useAction = <TInput, TOutput>(
+export const useAction = <TInput, TOutput> (
     action: Action<TInput, TOutput>,
-    options: UseActionOptions<TOutput> = {},
+    options: UseActionOptions<TOutput> = {}
 ) => {
-    const [fieldErrors, setFieldErrors] = useState<FieldErrors<TInput> | undefined>(undefined);
+    const [fieldErrors, setFieldErrors] = useState<FieldErrors<TInput> | undefined>(
+        undefined
+    );
     const [error, setError] = useState<string | undefined>(undefined);
     const [data, setData] = useState<TOutput | undefined>(undefined);
     const [isLoading, setIsLoading] = useState<boolean>(false);
 
     const execute = useCallback(
         async (input: TInput) => {
-            setIsLoading(true);
-            try {
-                const result = await action(input);
-                if (!result) {
-                    return;
-                }
-              
-                setFieldErrors(result.fieldErrors);
-                
-                if (result.error) {
-                    setError(result.error);
-                    options.onError?.(result.error);
-                }
-                if (result.data) {
-                    setData(result.data);
-                    options.onSuccess?.(result.data);
-                }
-            } finally {
-                setIsLoading(false);
-                options.onComplete?.();
+          setIsLoading(true);
+    
+          try {
+            const result = await action(input);
+    
+            if (!result) {
+              return;
             }
+    
+            setFieldErrors(result.fieldErrors);
+    
+            if (result.error) {
+              setError(result.error);
+              options.onError?.(result.error);
+            }
+    
+            if (result.data) {
+              setData(result.data);
+              options.onSuccess?.(result.data);
+            }
+          } finally {
+            setIsLoading(false);
+            options.onComplete?.();
+          }
         },
         [action, options]
-    );
-    return {
-        execute,
-        fieldErrors,
-        error,
-        data,
-        isLoading
-    };
-};
+        );
+
+        return {
+          execute,
+          fieldErrors,
+          error,
+          data,
+          isLoading,
+        };
+      };
