e22524a6d4b1456412da0d0b9e2922b3e89823e5
AlexandraM <76844097+Alexandra2888@users.noreply.github.com>
2024-05-16 17:56:57 +0300

    Add card actions

--- actions/update-card/schema.ts
+++ actions/update-card/schema.ts
@@ -2,18 +2,18 @@ import { z } from "zod";
 
 export const UpdateCard = z.object({
     boardId: z.string(),
-    desciption: z.optional(z.string({
+    description: z.optional(z.string({
         required_error: "Description is required",
         invalid_type_error: "Description is required",
     }).min(3, {
         message:"Description is too short"
     }),
 ),
-    title: z.string({
+    title:z.optional(z.string({
         required_error: "Title is required",
         invalid_type_error: "Title is required"
     }).min(3, {
         message: "Title is too short"
-    }),
+    })),
     id: z.string(),
 })
